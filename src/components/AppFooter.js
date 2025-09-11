8b6d1cf024d55e7a0d8b12b6793be37b55a8a038
João Jamba <jambapedrojoao@gmail.com>
2025-09-11 17:36:52 +0100

    footer description

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -6,14 +6,14 @@ const AppFooter = () => {
     <CFooter className="px-4">
       <div>
         <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+          DCG Edu
         </a>
-        <span className="ms-1">&copy; 2025 creativeLabs.</span>
+        <span className="ms-1">&copy; 2025 .</span>
       </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
         <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI React Admin &amp; Dashboard Template
+          V 1.0.21
         </a>
       </div>
     </CFooter>
